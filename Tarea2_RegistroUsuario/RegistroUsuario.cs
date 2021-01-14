dc5ed64d6ccf01a1d67e4a8a147eb2c35122b3a5
katiuska Labrada <katiuska.labrada.evangelista13@outlook.com>
2021-01-13 21:42:22 -0400

    Add project files.

---
+++ Tarea2_RegistroUsuario/RegistroUsuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea2_RegistroUsuario
+{
+    public partial class RegistroUsuario : Form
+    {
+        public RegistroUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fem_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
